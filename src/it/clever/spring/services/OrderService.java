a5170752a2d33ed7351b94dbb8d8ff4b51aaf93b
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-02-13 16:39:35 +0100

    i metodi che
    avvengono prima e dopo ... o sulle eccezioni...

--- src/it/clever/spring/services/OrderService.java
+++ src/it/clever/spring/services/OrderService.java
@@ -11,6 +11,9 @@ package it.clever.spring.services;
 
 import java.util.List;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +24,34 @@ import it.clever.spring.utils.RequiredTx;
  * @author robgion
  *
  */
+
+// tutte le classi che estendono LoggableAopService
+
+// cosa avviene prima e dopo viene avviato ... 
+
+//@Before("logPointCut()")
+//public void logBefore(JoinPoint joinPoint) {
+//
+//	System.out.println("\n\nlogBefore() is running!");
+//	System.out.println("hijacked : " + joinPoint.getSignature().getName());
+//	System.out.println("******");
+//}
+//
+//// dopo... 
+//
+//@After("logPointCut()")
+//public void logAfter(JoinPoint joinPoint) {
+//
+//	System.out.println("\n\nlogAfter() is running!");
+//	System.out.println("hijacked : " + joinPoint.getSignature().getName());
+//	System.out.println("******");
+//
+//}
+
+// del file logserviceAspect... 
+
+
+
 public interface OrderService extends LoggableAopService {
 
 	public Order createNewOrder(Long customerId, Long productId, int quantity, double amount) throws Exception;
